46312ecae5fb9da89fd1cfc534ad3f7cd1b50844
Mateus Stangherlin <mateus_stangherlin@hotmail.com>
2023-05-16 13:55:53 -0300

    first commit

---
+++ app/validacao.js
@@ -0,0 +1,47 @@
+function verificaChute(chute) {
+    const numero = +chute
+
+    if (chuteInvalido(numero)) {
+        elementoChute.innerHTML += '<div>valor invalido</div>'
+        return
+    }
+
+    if (numeroMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor Inválido: o numero precisa ser entre ${menorValor} a ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h1>Você Acertou!!</h1>
+            <h2>O número secreto era ${numeroSecreto}</h2>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        ` 
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+
+
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
